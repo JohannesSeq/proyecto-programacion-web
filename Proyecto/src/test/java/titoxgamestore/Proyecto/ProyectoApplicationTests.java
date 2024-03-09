180fa93ed0785653d8d3436bb967a59d025cd244
Sequeira Fernandez Johannes Armando <84215987+JohannesSeq@users.noreply.github.com>
2024-03-09 15:30:58 -0600

    Inicializacion

---
+++ Proyecto/src/test/java/titoxgamestore/Proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package titoxgamestore.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
