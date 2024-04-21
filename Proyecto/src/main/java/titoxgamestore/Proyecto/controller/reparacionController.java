440768fbe2c452e9ff71929aebbb6d1b19413db4
Daraya29 <42704650+Jotadeiru@users.noreply.github.com>
2024-04-20 19:34:42 -0600

    Arreglando bug

--- Proyecto/src/main/java/titoxgamestore/Proyecto/controller/reparacionController.java
+++ Proyecto/src/main/java/titoxgamestore/Proyecto/controller/reparacionController.java
@@ -1,24 +1,24 @@
 
 package titoxgamestore.Proyecto.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import titoxgamestore.Proyecto.service.reparacionService;
 
 @Controller
-@Slf4j
 @RequestMapping("/FormReparaciones")
 public class reparacionController {
     
     @Autowired
-    private reparacionService reparacioService; 
-    
-    public String inicio(Model model){
-        var reparaciones = reparacionService.getreparacion(false);
+    private reparacionService reparacioService;
 
-        return null;
+    @GetMapping("/listado")
+    private String listado(Model model){
+        var reparaciones = reparacioService.getreparacion(false);
+
+        return "/reparaciones/listado";
     }
 }
--- Proyecto/src/main/java/titoxgamestore/Proyecto/service/reparacionService.java
+++ Proyecto/src/main/java/titoxgamestore/Proyecto/service/reparacionService.java
@@ -4,11 +4,7 @@ package titoxgamestore.Proyecto.service;
 import java.util.List;
 import titoxgamestore.Proyecto.domain.reparacion;
 
-
-
-
-
 public interface reparacionService {
 
-   public static List<reparacion> getreparacion(boolean activos);
+   public List<reparacion> getreparacion(boolean activos);
 }
