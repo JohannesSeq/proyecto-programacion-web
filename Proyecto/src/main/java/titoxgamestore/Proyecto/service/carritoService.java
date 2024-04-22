4feaae8fad5a27b68b3555a6b13c875f5d704e4a
Daraya29 <42704650+Jotadeiru@users.noreply.github.com>
2024-04-22 10:50:07 -0600

    Agregando lógica del carrito

--- Proyecto/src/main/java/titoxgamestore/Proyecto/service/carritoService.java
+++ Proyecto/src/main/java/titoxgamestore/Proyecto/service/carritoService.java
@@ -1,4 +1,12 @@
 package titoxgamestore.Proyecto.service;
 
+import titoxgamestore.Proyecto.domain.Item;
+
+import java.util.List;
+
 public interface carritoService {
+    void addToCart(Item item);
+    void removeFromCart(Long itemId);
+    void updateItemInCart(Item item);
+    List<Item> getCartItems();
 }
